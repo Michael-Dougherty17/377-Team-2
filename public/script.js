bedcf69cb5e8651744ad6aed34b0b75168febc97
Michael-Dougherty17 <mdough28@terpmail.umd.edu>
2020-12-08 17:43:06 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -15,3 +15,24 @@ fetch(busData)
     .then((blob) => blob.json())
     .then((data) => buses.push(...data));
 
+<<<<<<< Updated upstream
+=======
+async function main() {
+  try {
+    //load bus data
+    const busData = "https://api.umd.io/v1/bus/routes/104";
+    const data = await fetch(busData);
+    const buses = await data.json();
+    console.log(buses);
+    //generate li, inject into select element
+    //name attrib and value for each
+    //labs 5+6
+    //use button to fire event or submission event lisener on form
+  } 
+  
+  catch (error) {
+    console.log(error);
+  }
+}
+window.onload = main;
+>>>>>>> Stashed changes
