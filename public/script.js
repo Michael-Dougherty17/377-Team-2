601ee7374dbf293cb6c3f5a8bd7d9dd9fefc070a
Swapnil Goplani <swapnilgoplani@gmail.com>
2020-12-08 18:04:51 -0500

    trying to get the api data in the form
    gonna have to store stops in a big array or something to search thru them ourselves

--- public/script.js
+++ public/script.js
@@ -1,7 +1,31 @@
 //mobile dropdown
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
+const burgerIcon = document.querySelector("#burger");
+const navbarMenu = document.querySelector("#nav-links");
 
-burgerIcon.addEventListener('click', () => {
-    navbarMenu.classList.toggle('is-active');
-})
+burgerIcon.addEventListener("click", () => {
+  navbarMenu.classList.toggle("is-active");
+});
+
+async function main() {
+  try {
+    //load bus data
+    const busData = "https://api.umd.io/v1/bus/stops";
+    const data = await fetch(busData);
+    const buses = await data.json();
+    console.log(buses);
+
+    //generate li, inject into select element
+    //name attrib and value for each
+    //labs 5+6
+    //use button to fire event or submission event lisener on form
+
+    //fetch stop list from api, then store the stops in a "thing" so that we 
+    //can search via title(since api doesnt allow for search by title) ourselves and then get the stop id and return that to the api to
+    //get estimated arrival times
+  } 
+  
+  catch (error) {
+    console.log(error);
+  }
+}
+window.onload = main;
