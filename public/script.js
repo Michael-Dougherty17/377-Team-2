dcc9a33f203ea041f9de33e8129bfb65f5a5080a
Swapnil Goplani <swapnilgoplani@gmail.com>
2020-12-10 16:45:48 -0500

    post meeting with professor
    had to do some fishy stuff but its gonna work as intended

--- public/script.js
+++ public/script.js
@@ -1,56 +1,60 @@
 //mobile dropdown
 const burgerIcon = document.querySelector("#burger");
 const navbarMenu = document.querySelector("#nav-links");
+const form = document.querySelector(".routeInfo");
+let startInputID = null;
+let endInputID = null;
 
 burgerIcon.addEventListener("click", () => {
   navbarMenu.classList.toggle("is-active");
 });
 
+
+
 async function main() {
   try {
     //load bus data
-    const busData = "https://api.umd.io/v1/bus/stops";
-    const data = await fetch(busData);
-    const buses = await data.json();
-    //console.log(buses);
-    for (let i = 0; i < buses.length; i++) {
+    const routeData = "https://api.umd.io/v1/bus/routes";
+    const data = await fetch(routeData);
+    const routes = await data.json();
+    console.log(routes);
 
-      //console.log(buses[i].title)
+    const stopData = "https://api.umd.io/v1/bus/stops";
+    const dataTwo = await fetch(stopData);
+    const stopArray = await dataTwo.json();
 
-      if(buses[i].title == "La Plata Hall"){
-        //console.log(buses[i].stop_id);
-        let results = buses[i].stop_id;
-        console.log(results);
-        const arrivalTime = `https://api.umd.io/v1/bus/stops/${results}`;
-        const dataTwo = await fetch(arrivalTime);
-        const arrival = await dataTwo.json();
-        console.log(arrival);
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const formData = $(event.target).serializeArray();
+      const startInput = formData[0].value;
+      const endInput = formData[1].value;
+      
+      for (let j = 0; j < stopArray.length; j++) {
+
+        if (startInput == stopArray[j].title) {
+          startInputID = stopArray[j].stop_id;
+        }
+
+        if (endInput == stopArray[j].title) {
+          endInputID = stopArray[j].stop_id;
+        }
       }
 
-      function testFunction() {
+      console.log(startInputID);
+    })
 
-        const startInput = document.getElementById('start').value;
-        const endInput = document.getElementById('end').value;
-        console.log(startInput);
+    for (let i = 0; i < routes.length; i++) {
 
-        //how to send data to backend and work with it and then send data back to front end so I can change lines 59 and 61 with said data
-      }
-
-      //lines 105+ in lab 5 to select user input and use that as arguement instead of hard coding line 20
-
+      const routeIDCall = routes[i].route_id;
+      const routeData = `https://api.umd.io/v1/bus/routes/${routeIDCall}`;
+      const dataThree = await fetch(routeData);
+      const useableRouteData = await dataThree.json();
+      let stopIDArray = useableRouteData[0].stops;
+      console.log(stopIDArray);
+      //I have the array of stops now I need to see if the stop_id matches the starting and ending stop id and if it does I can
+      //call the api with the route id and take the stop id to get estimated arrival time
       
-
-
-      
-
     }
-    //generate li, inject into select element
-    //name attrib and value for each
-    //labs 5+6
-    //use button to fire event or submission event lisener on form
-    //fetch stop list from api, then store the stops in a "thing" so that we 
-    //can search via title(since api doesnt allow for search by title) ourselves and then get the stop id and return that to the api to
-    //get estimated arrival times
   } 
   
   catch (error) {
