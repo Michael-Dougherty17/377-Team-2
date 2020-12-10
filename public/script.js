f9315a4b14aae15fa873846ba247fd776f691577
Swapnil Goplani <swapnilgoplani@gmail.com>
2020-12-10 17:22:05 -0500

    theres a problem with line 47 of the script
    it was working before and would call the api in the for loop but now it doesnt want to for some reason. There might be too much code in between it or maybe the nested for loop messed it up

--- public/script.js
+++ public/script.js
@@ -1,15 +1,15 @@
-//mobile dropdown
 const burgerIcon = document.querySelector("#burger");
 const navbarMenu = document.querySelector("#nav-links");
 const form = document.querySelector(".routeInfo");
 let startInputID = null;
 let endInputID = null;
+let cashNow = 0;
+let noMoreThanOne = 0;
+let myRoute = "hello world";
 
 burgerIcon.addEventListener("click", () => {
   navbarMenu.classList.toggle("is-active");
 });
-
-
 
 async function main() {
   try {
@@ -17,8 +17,6 @@ async function main() {
     const routeData = "https://api.umd.io/v1/bus/routes";
     const data = await fetch(routeData);
     const routes = await data.json();
-    console.log(routes);
-
     const stopData = "https://api.umd.io/v1/bus/stops";
     const dataTwo = await fetch(stopData);
     const stopArray = await dataTwo.json();
@@ -39,22 +37,37 @@ async function main() {
           endInputID = stopArray[j].stop_id;
         }
       }
+      for (let i = 0; i < routes.length; i++) {
 
-      console.log(startInputID);
+        cashNow = 0;
+
+        const routeIDCall = routes[i].route_id;
+        const routeData = `https://api.umd.io/v1/bus/routes/${routeIDCall}`;
+        const dataThree = await fetch(routeData);
+        const useableRouteData = await dataThree.json();
+        let stopIDArray = useableRouteData[0].stops;
+
+        for (let n = 0; n < stopIDArray.length; n++) {
+
+          if (stopIDArray[n] == startInputID) {
+            cashNow += 1;
+          }
+          
+          if (stopIDArray[n] == endInputID) {
+            cashNow += 1;
+          }
+
+        }
+        
+        if (noMoreThanOne == 0 && cashNow == 2) {
+
+          noMoreThanOne = 1;
+          myRoute = routeIDCall;
+        }
+      }
+
+      console.log(myRoute);
     })
-
-    for (let i = 0; i < routes.length; i++) {
-
-      const routeIDCall = routes[i].route_id;
-      const routeData = `https://api.umd.io/v1/bus/routes/${routeIDCall}`;
-      const dataThree = await fetch(routeData);
-      const useableRouteData = await dataThree.json();
-      let stopIDArray = useableRouteData[0].stops;
-      console.log(stopIDArray);
-      //I have the array of stops now I need to see if the stop_id matches the starting and ending stop id and if it does I can
-      //call the api with the route id and take the stop id to get estimated arrival time
-      
-    }
   } 
   
   catch (error) {
