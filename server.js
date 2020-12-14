8da09d9dc71fa088644a33a824f59155a5929a45
ulrich-97 <63929467+ulrich-97@users.noreply.github.com>
2020-12-14 11:04:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,50 @@ app.use((req, res, next) => {
 });
 
 app.route('/api')
-  .get(async (req, res) => {
+  .get(async (req, res) => {/* eslint-disable no-console */
+    /* eslint-disable no-unused-vars */
+    import express from 'express';
+    import dotenv from 'dotenv';
+    import fetch from 'node-fetch';
+    
+    dotenv.config();
+    
+    const app = express();
+    const port = process.env.PORT || 3000;
+    
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(express.static('public'));
+    
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      next();
+    });
+    
+    app.route('/api')
+      .get(async (req, res) => {
+        const data = await fetch('https://api.umd.io/v1/bus/routes');
+        const json = await data.json();
+        res.json(json);
+      })
+      
+      .post(async (req, res) => {
+        var request = new XMLHttpRequest();
+        console.log('POST request detected');
+        console.log('Form data in res.body', req.body);
+    
+        const data = await fetch('https://api.umd.io/v1/bus/routes');
+        const json = await data.json();
+        console.log('data from fetch', json);
+        res.json(json);
+      });
+    
+      
+    
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}!`);
+    });
     console.log('GET request detected');
     console.log('Fetch request data', json);
   })
